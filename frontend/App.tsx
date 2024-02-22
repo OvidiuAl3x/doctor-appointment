458f46e06e17102e89b85ea997843cf76b708168
OvidiuAl3x <alexandruovidiu1999@gmail.com>
2024-02-22 12:24:00 +0200

    fix

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import Home from "./components/home";
+import Home from "./components/Home";
 
 export default function App() {
   return (
